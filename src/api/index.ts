e952ec8ecfc56eadd931e5b0515f57fbbf5f2088
Moonded <moonded@moonded.com>
2024-08-26 17:43:32 +0200

    Update api endpoint - Part 1

--- src/api/index.ts
+++ src/api/index.ts
@@ -9,13 +9,15 @@ import experimental from "./experimental";
 import endpoint from "./endpoint";
 import auth from "./auth";
 import bot from "./bot";
+import dev from "./dev";
 
 router.use("/metrics", metrics);
 router.use("/discord", apiToken, discord);
 router.use("/experimental", apiToken, experimental);
 router.use("/endpoint", apiToken, endpoint);
 router.use("/bot", apiToken, bot);
-router.use("/auth", apiToken, auth);
+// router.use("/dev", apiToken, dev);
+// router.use("/auth", apiToken, auth);
 
 router.get("/", (req, res) => {
   res.sendStatus(200);
