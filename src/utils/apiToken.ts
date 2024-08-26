26e6a7a477bf05ae8cb56e184b78e80766a66ec5
Moonded <moonded@moonded.com>
2024-08-26 17:45:57 +0200

    update tokenHandler

--- src/utils/apiToken.ts
+++ src/utils/apiToken.ts
@@ -1,18 +1,8 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-// const exception = [
-//   "/api/metrics",
-//   "/api",
-// ];
-
 export function apiToken(req: Request, res: Response, next: any) {
-  // const url = req.originalUrl;
   const headers = req.headers["authorization"];
-
-  // if (exception.includes(url)) {
-  //   return next();
-  // }
 
   if (!headers) {
     return res.status(401).send("Unauthorized");
