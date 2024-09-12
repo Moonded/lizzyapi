c832f09fd6023d8f59003c262bbcda9ab4f419cf
Moonded <moonded@moonded.com>
2024-09-12 17:07:56 +0200

    chore: Remove unused files and dependencies

--- src/utils/bitfield.ts
+++
@@ -1,2 +0,0 @@
-import Bitfield from "bitfield";
-export const field = new Bitfield(512); // Create a bitfield with 256 bits.
--- src/utils/server.ts
+++
@@ -1,5 +0,0 @@
-import fs from "node:fs";
-
-export const readFile = (path: string) => {
-  return fs.readFileSync("src/public/" + path, "utf-8");
-};
