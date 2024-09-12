84f3b53658103c7a1b19ff62fe3bf10cb5f1641a
Moonded <moonded@moonded.com>
2024-09-12 17:07:35 +0200

    feat: Add types for GitHub and middleware

---
+++ src/types/github.d.ts
@@ -0,0 +1,44 @@
+interface GraphData {
+  red4ext: {
+    latestRelease: {
+      tagName: string;
+      updatedAt: string;
+      url: string;
+    };
+  };
+  archivexl: {
+    latestRelease: {
+      tagName: string;
+      updatedAt: string;
+      url: string;
+    };
+  };
+  tweakxl: {
+    latestRelease: {
+      tagName: string;
+      updatedAt: string;
+      url: string;
+    };
+  };
+  codeware: {
+    latestRelease: {
+      tagName: string;
+      updatedAt: string;
+      url: string;
+    };
+  };
+  cet: {
+    latestRelease: {
+      tagName: string;
+      updatedAt: string;
+      url: string;
+    };
+  };
+  redscript: {
+    latestRelease: {
+      tagName: string;
+      updatedAt: string;
+      url: string;
+    };
+  };
+};
