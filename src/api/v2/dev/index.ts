41d157519dc78e9fe76a6cf02d032484244bdc4a
Moonded <moonded@moonded.com>
2024-09-18 21:34:44 +0200

    chore: Refactor API endpoints and update dependencies

--- src/api/v2/dev/index.ts
+++ src/api/v2/dev/index.ts
@@ -1,10 +1,10 @@
 import express from "express";
 const router = express.Router();
-import bitfield from "./bitfield";
+
 import user from "./user";
 import perms from "./perms";
 
-router.use("/bitfield", bitfield);
+
 router.use("/user", user);
 router.use("/perms", perms);
 
