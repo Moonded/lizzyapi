bc8a05ed3cfa1bcb7ec429142c1f4da4b071edf2
Moonded <moonded@moonded.com>
2024-09-19 19:46:23 +0200

    feat: Add GitHub contributions API endpoint for development

--- src/api/v2/dev/index.ts
+++ src/api/v2/dev/index.ts
@@ -3,8 +3,9 @@ const router = express.Router();
 
 import user from "./user";
 import perms from "./perms";
+import github_contribs from "./github_contribs";
 
-
+router.use("/git", github_contribs)
 router.use("/user", user);
 router.use("/perms", perms);
 
