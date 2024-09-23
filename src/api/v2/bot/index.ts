5ce16685a96099dcb1fbfdafb56a8fd8345d20c6
Moonded <moonded@moonded.com>
2024-09-23 20:55:03 +0200

    Refactor: Remove unused team API routes

--- src/api/v2/bot/index.ts
+++ src/api/v2/bot/index.ts
@@ -2,9 +2,7 @@ import express from "express";
 const router = express.Router();
 
 import commands from "./commands";
-import team from "./team";
 
 router.use("/commands", commands);
-router.use("/team", team);
 
 export default router;
